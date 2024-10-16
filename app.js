ad4105117f1a5a4f8bf20c4f10090f943e55f21d
Tsgwala <thobanisibonakaliso@gmail.com>
2024-10-16 11:23:08 +0200

    deploy retry #4

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.post('/analyze', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 });
